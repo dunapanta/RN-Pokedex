7d77aff3a5882b915a6a194d3278b017e1d132d3
Daniel <dunapanta2@gmail.com>
2021-06-03 12:23:04 -0500

    Adding image on HomeSreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {globalStyles} from '../theme/appTheme';
 
 export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={globalStyles.pokebolaBG}
+      />
+      <Text
+        style={{
+          ...globalStyles.title,
+          ...globalStyles.globalMargin,
+          top: top + 20,
+        }}>
+        Pokedex
+      </Text>
+    </>
   );
 };
