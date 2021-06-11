fe571158f58685d9aed3c0f1101559c2243dcd75
Daniel <dunapanta2@gmail.com>
2021-06-11 15:55:53 -0500

    Image Pokemon not found

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,6 +1,15 @@
 import {DrawerScreenProps} from '@react-navigation/drawer';
 import React, {useEffect, useState} from 'react';
-import {Dimensions, FlatList, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {Loading} from '../components/Loading';
@@ -70,15 +79,36 @@ export const SearchScreen = ({navigation}: Props) => {
 
       <SearchInput onDebounce={value => setTerm(value)} />
 
-      <FlatList
-        data={pokemonFiltred}
-        keyExtractor={pokemon => pokemon.id}
-        showsVerticalScrollIndicator={false}
-        numColumns={2}
-        //Header
+      {term && pokemonFiltred.length === 0 ? (
+        <View
+          style={{
+            paddingTop: 60,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Image
+            source={require('../assets/poke-warning.png')}
+            style={{
+              opacity: 0.7,
 
-        renderItem={({item, index}) => <PokemonCard pokemon={item} />}
-      />
+              width: 240,
+              height: 210,
+            }}
+          />
+          <Text style={{fontSize: 20, marginTop: 10}}>
+            Pokemon no encontrado
+          </Text>
+        </View>
+      ) : (
+        <FlatList
+          data={pokemonFiltred}
+          keyExtractor={pokemon => pokemon.id}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          //Header
+          renderItem={({item, index}) => <PokemonCard pokemon={item} />}
+        />
+      )}
     </View>
   );
 };
